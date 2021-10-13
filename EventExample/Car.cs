c19829dd71b048697ee1cf8fc8cb7f6b007779dd
VladislavZverincev <hopmxod007@mail.ru>
2021-10-13 17:44:37 +0700

    Добавьте файлы проекта.

---
+++ EventExample/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExample
+{
+    class Car
+    {
+        public event Action DoorsOpened;
+        public event EventHandler<CarEventArgs> DoorsClosed;
+
+        public string Mark { get; set; }
+        public string Number { get; set; }
+        public string Owner { get; set; }
+        public Car(string _mark, string _number, string _owner)
+        {
+            Mark = _mark;
+            Number = _number;
+            Owner = _owner;
+        }
+        public void OpenDoors()
+        {
+            DoorsOpened?.Invoke();
+        }
+        public void CloseDoors()
+        {
+            var eArgs = new CarEventArgs(Number, Owner);
+            DoorsClosed?.Invoke(this, eArgs);
+        }
+    }
+    public class CarEventArgs : EventArgs
+    {
+        public CarEventArgs(string _number, string _owner)
+        {
+            Number = _number;
+            Owner = _owner;
+        }
+        public string Number { get; set; }
+        public string Owner { get; set; }
+    }
+}
---
+++ EventExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Honda", "7EK77H.24", "Vlad");
+            car.DoorsOpened += OnDoorsOpened;
+            //Or Lambda
+            //car.DoorsOpened += (arguments if have) => { some logic };
+            
+            car.DoorsClosed += (object sender, CarEventArgs e) => 
+            { if (sender is Car) { Console.WriteLine($"Doors in {((Car)sender).Mark} " +
+                $"has been closed!"); } };
+            
+            car.OpenDoors();
+            car.CloseDoors();
+            Console.ReadLine();
+            
+        }
+
+        static void OnDoorsOpened()
+        {
+            Console.WriteLine("Doors has been opened!");
+        }
+    }
+}
